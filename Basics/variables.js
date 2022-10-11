ad2e14ed7125edf8d017a3b551289a0792f350d2
Akash Srivastava <ankitsri66610@gmail.com>
2022-10-11 16:56:11 +0530

    Variables example

--- Basics/variables.js
+++ Basics/variables.js
@@ -1,7 +1,9 @@
 a = 20; // Global scope
 var b = 30; // Here, global scope but if in a fn. then function scope
 let c = 40; // Block scope
+d = 50;
 
 console.log(a);
 console.log(b);
 console.log(c);
+console.log(d);
