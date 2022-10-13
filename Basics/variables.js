48f4d6ea7e0a38bf98439fa8839a9e8a3fc2a534
Akash Srivastava <ankitsri66610@gmail.com>
2022-10-13 11:56:16 +0530

    no error

--- Basics/variables.js
+++ Basics/variables.js
@@ -2,6 +2,7 @@ a = 20; // Global scope
 var b = 30; // Here, global scope but if in a fn. then function scope
 let c = 40; // Block scope
 d = 50;
+let e = [1,2,3,4,5];
 
 console.log(a);
 console.log(b);
