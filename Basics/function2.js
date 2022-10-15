67be30653f560dc965dc7a388d5c921de4529328
Akash Srivastava <ankitsri66610@gmail.com>
2022-10-15 14:31:52 +0530

    variable lenght arguments

--- Basics/function2.js
+++ Basics/function2.js
@@ -6,7 +6,14 @@ function sum(a,b) {
     return a + b;
 }
 
+function sum2(c,d) {
+    return c + d;
+}
+
 hello("hello world");
+
 result = sum(3,4);
+result2 = sum2(4); // variable length argument
 
 console.log(result);
+console.log(result2); 
